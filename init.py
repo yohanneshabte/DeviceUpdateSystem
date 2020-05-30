4c795c64d56da8147fc838279f229873b7356cf5
Yohannes <yohannes.habte@mnsu.edu>
2020-05-29 23:41:53 -0500

    setup init

--- init.py
+++ init.py
@@ -3,3 +3,26 @@
 #getDriveInfo() : object/list
 #getProjects() : object
 
+
+class Project:
+    name = ""
+    dev = ""
+    date = ""
+    def __init__(self, name, dev, date):
+        self.name = name
+        self.dev = dev
+        self.date = date
+
+class Drive:
+    letter = ""
+    name = ""
+    totalSize = 0
+    freeSpace = 0
+    driveType = ""
+    def __init__(self, letter, name, totalSize, freeSpace, driveType):
+        self.letter = letter
+        self.name = name
+        self.totalSize = totalSize
+        self.freeSpace = freeSpace
+        self.driveType = driveType
+
